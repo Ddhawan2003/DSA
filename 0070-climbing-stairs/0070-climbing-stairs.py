e5864512707d155d31d64c7b9ea704d9e9eb2add
Ddhawan2003 <89482716+Ddhawan2003@users.noreply.github.com>
2024-05-24 18:52:18 +0530

    Time: 34 ms (59.09%), Space: 16.4 MB (83.91%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n==0: 
+            return 0
+        if n==1:
+            return 1
+        if n==2: 
+            return 2
+        else:
+            dp = [0] * (n + 1)
+            dp[1] = 1
+            dp[2] = 2
+            # Fill the dp array
+            for i in range(3, n + 1):
+                dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
