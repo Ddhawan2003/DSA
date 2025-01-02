5965faf5782a0a2c593f48193b1453f3d6d4199f
Ddhawan2003 <89482716+Ddhawan2003@users.noreply.github.com>
2025-01-02 12:23:04 -0500

    Time: 2072 ms (31.59%), Space: 13.4 MB (5.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,19 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # for i in range(0, len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if (nums[i] + nums[j]) == target:
-        #             return [i,j]
-        nums_with_index = [(number, index) for index, number in enumerate(nums)]
-        nums_with_index.sort(key=lambda x: x[0])
-        left = 0
-        right = len(nums) - 1
-        while left < right:
-            current_sum = nums_with_index[left][0] + nums_with_index[right][0]
-            if current_sum == target:
-                return [nums_with_index[left][1], nums_with_index[right][1]]
-            if current_sum < target:
-                left = left + 1
-            if current_sum > target:
-                right = right - 1
+        # left = 0
+        # right = len(nums)-1
+        # while left <= right:
+        #     if nums[left] + nums[right] == target:
+        #         break
+        #     elif abs(nums[left]) + abs(nums[right]) < abs(target):
+        #         left+=1
+        #     else:
+        #         right-=1
+        # return [left, right]
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+        
