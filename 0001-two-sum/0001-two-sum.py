c99dabfb738ba6bdf7605658b03a3b979c902fd1
Ddhawan2003 <89482716+Ddhawan2003@users.noreply.github.com>
2025-03-09 06:04:45 -0400

    Time: 0 ms (100%), Space: 13.1 MB (62.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,20 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # left = 0
-        # right = len(nums)-1
-        # while left <= right:
-        #     if nums[left] + nums[right] == target:
-        #         break
-        #     elif abs(nums[left]) + abs(nums[right]) < abs(target):
-        #         left+=1
-        #     else:
-        #         right-=1
-        # return [left, right]
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []
+        index_map = {}  # Store value -> index mapping
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in index_map:
+                return [index_map[complement], i]
+            index_map[num] = i  # Store index of current number
 
         
