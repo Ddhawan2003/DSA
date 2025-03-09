b73857d07eb1bbfba97e0b40ad4e7dfcad616900
Ddhawan2003 <89482716+Ddhawan2003@users.noreply.github.com>
2025-03-09 06:45:25 -0400

    Time: 11 ms (83.54%), Space: 20.6 MB (89.54%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,8 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+        res=defaultdict(list)
         for s in strs:
-            sortedS = ''.join(sorted(s))
-            res[sortedS].append(s)
+            # count = [0] * 26  # ✅ Fixed-size array for character counts
+            # for char in s:
+            #     count[ord(char) - ord('a')] += 1  # ✅ Map 'a' -> 0, 'b' -> 1, etc.
+            # res[tuple(count)].append(s)  # ✅ Use tuple as key
+            temp=''.join(sorted(s))
+            res[temp].append(s)
         return list(res.values())
         
