f2db80bc7950f8fcfdd223765732735900adc07a
Ddhawan2003 <89482716+Ddhawan2003@users.noreply.github.com>
2025-03-12 15:47:33 -0400

    Time: 0 ms (100%), Space: 17.8 MB (48.11%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -3,35 +3,41 @@ class Solution:
         maxI=2**31-1
         minI=(-2)**31
         sign = 1
-        res = ''
-        flag=1
+        res = []
+        flag=True
         s = s.lstrip()
         if not s:
             return 0
-        for c in s:
-            if flag:
-                if c == "-":
-                    sign = -1
-                    flag = 0
-                elif not c.isdigit():
-                    if c == "+":
-                        flag=0
-                        continue
-                    break
-                else:
-                    flag=0
-                    res+=c
-            else:
-                if c.isdigit():
-                    res+=c
-                else:
-                    break
-        if res:
-            res = int(res)
-        else:
+        # for c in s:
+        #     if flag:
+        #         if c == "-":
+        #             sign = -1
+        #             flag = 0
+        #         elif not c.isdigit():
+        #             if c == "+":
+        #                 flag=0
+        #                 continue
+        #             break
+        #         else:
+        #             flag=0
+        #             res+=c
+        #     else:
+        #         if c.isdigit():
+        #             res+=c
+        #         else:
+        #             break
+        i = 0
+        if s[i] in "+-":
+            sign = -1 if s[i] == '-' else 1
+            i += 1
+        while i < len(s) and s[i].isdigit():
+            res.append(s[i])
+            i += 1
+        if not res:
             return 0
-        if sign*res > maxI:
-            res=maxI
-        elif sign*res < minI:
-            res = abs(minI)
-        return sign*res
+        res = int(''.join(res)) * sign
+        if res > maxI:
+            return maxI
+        if res < minI:
+            return minI
+        return res
