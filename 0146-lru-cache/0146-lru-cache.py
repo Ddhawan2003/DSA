63c0c074063d7947ae378cd7dbc1b1471f3af0aa
Ddhawan2003 <89482716+Ddhawan2003@users.noreply.github.com>
2025-04-05 17:37:57 -0400

    Time: 137 ms (61.21%), Space: 78.6 MB (27.58%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,60 +1,60 @@
 class Node:
-    def __init__(self, key, val):
+    def __init__(self, key: int, value: int):
         self.key = key
-        self.val = val
+        self.value = value
         self.prev = None
         self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.cache = {}  
         self.capacity = capacity
-        self.cache = {}  # key -> Node
-
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
+        self.size = 0
+        self.head = Node(0,0)
+        self.tail = Node(0,0)
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def _add(self, node):
-        nxt = self.head.next
-        self.head.next = node
-        node.prev = self.head
-        node.next = nxt
-        nxt.prev = node
-
-    def _remove(self, node):
-        prev = node.prev
-        nxt = node.next
-        prev.next = nxt
-        nxt.prev = prev
-
-    def _move_to_head(self, node):
-        self._remove(node)
-        self._add(node)
-
-    def _pop_tail(self):
-        lru = self.tail.prev
-        self._remove(lru)
-        return lru
-
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        node = self.cache[key]
-        self._move_to_head(node)
-        return node.val
+        if key in self.cache:
+            node = self.cache[key]
+            self.moveFront(node)
+            return node.value
+        return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
-            node.val = value
-            self._move_to_head(node)
+            node.value = value
+            self.moveFront(node)
         else:
-            node = Node(key, value)
-            self.cache[key] = node
-            self._add(node)
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self.addToFront(new_node)
+            self.size+=1
 
-            if len(self.cache) > self.capacity:
-                lru = self._pop_tail()
+            if self.size > self.capacity:
+                lru = self.tail.prev
+                self.removeNode(lru)
                 del self.cache[lru.key]
+                self.size-=1
+
+    def removeNode(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    
+    def addToFront(self, node):
+        self.head.next.prev = node
+        node.next = self.head.next
+        self.head.next = node
+        node.prev = self.head
+
+    def moveFront(self, node):
+        self.removeNode(node)
+        self.addToFront(node)
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
