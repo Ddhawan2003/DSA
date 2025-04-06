4914783e06d2ce9259bf6ffb7f408e17f056a716
Ddhawan2003 <89482716+Ddhawan2003@users.noreply.github.com>
2025-04-06 00:33:54 -0400

    Time: 231 ms (90.3%), Space: 20.2 MB (69.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,50 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        islands = 0
+        # islands = 0
+        # rows = len(grid)
+        # cols = len(grid[0])
 
-        def dfs(r, c):
-            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == '0':
-                return
-            grid[r][c] = "0"
-            # Explore all 4 directions (Up, Down, Left, Right)
-            dfs(r + 1, c)  # Down
-            dfs(r - 1, c)  # Up
-            dfs(r, c + 1)  # Right
-            dfs(r, c - 1)  # Left
+        # def dfs(r,c):
+        #     if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == "0":
+        #         return
+        #     grid[r][c] = "0"
+        #     dfs(r-1,c)
+        #     dfs(r+1,c)
+        #     dfs(r,c-1)
+        #     dfs(r,c+1)
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if grid[i][j] == "1":
+        #             dfs(i,j)
+        #             islands+=1
+        # return islands
+
+        islands = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        def bfs(r,c):
+            queue = deque()
+            queue.append((r,c))
+            grid[r][c] == "0"
+
+            while queue:
+                row,col = queue.popleft()
+
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if (0<=nr<rows and 0<=nc<cols and grid[nr][nc]=="1"):
+                        grid[nr][nc] = '0'
+                        queue.append((nr,nc))
+
+
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == "1":
-                    dfs(i, j)
                     islands+=1
-            
+                    bfs(i,j)
         return islands
 
         
